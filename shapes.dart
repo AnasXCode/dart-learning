f2e590a9ff2b6c61938a11f23cbef2003fbe93a5
AnasXCode <anasahmed.appdev@gmail.com>
2025-03-24 18:07:07 +0500

    Create shapes
    Added a Dart program to print triangle and pentagon shapes using ASCII characters.

---
+++ shapes.dart
@@ -0,0 +1,20 @@
+void main() {
+  //Triangle Shape
+  print('            ***');
+  print('           *   *');
+  print('          *     *');
+  print('         *       *');
+  print('        *         *');
+  print('       *           *');
+  print('       *************');
+  //Pentagon Shape
+  print('                                *');
+  print('                             *      *');
+  print('                            *          *');
+  print('                          *              *');
+  print('                        *                   *');
+  print('                          *                *');
+  print('                            *            *');
+  print('                              *        *');
+  print('                               *******');
+}
