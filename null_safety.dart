fbdbf4ddb4f8311b23adfaaa1f73b84cd81d7454
AnasXCode <anasahmed.appdev@gmail.com>
2025-04-06 20:56:20 +0500

    Add files via upload
    How to use null safety and why we use we use it to assign value later.

---
+++ null_safety.dart
@@ -0,0 +1,11 @@
+void main() {
+  /*
+   There are two types of variables.
+   int age = 20;    non null variable
+   int age=;        null variable
+*/
+  int age = 20;
+  int? Age; // just put ? to handle it
+  print(age);
+  print(Age);
+}
